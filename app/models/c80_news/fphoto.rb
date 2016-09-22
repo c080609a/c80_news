010fcc047381ea6a0c2b6368204fcc44b58b679b
C80609A <c080609a@gmail.com>
2016-09-22 15:59:22 +0700

    master: модифицировал вызов content_image: добавил параметр, от которого зависит размер возвращаемой картинки

--- app/models/c80_news/fphoto.rb
+++ app/models/c80_news/fphoto.rb
@@ -12,24 +12,45 @@ module C80News
     # в ~ от размеров thumb-ов и page_content_width - выдать соответствующую картинку
     # • Если у картинки thumb_big шириной ≥ page_content_width - вставляем этот thumb_big.
     # • Иначе: вставлем thumb_small.
-    def content_image
-      img = MiniMagick::Image.open(image.thumb_big.path)
-      w = SiteProp.first.page_content_width
-      if img["width"] < w
+    def content_image(type='normal')
+
+      if type == 'normal'
+        img = MiniMagick::Image.open(image.thumb_big.path)
+        w = SiteProp.first.page_content_width
+        if img["width"] < w
+          image.thumb_small
+        else
+          image.thumb_big
+        end
+
+      elsif type == 'small'
         image.thumb_small
-      else
+
+      elsif type == 'big'
         image.thumb_big
       end
+
     end
 
     # выдать размеры картинки, которая будет вставлена в текст страницы
-    def content_image_size
-      img = MiniMagick::Image.open(image.thumb_big.path)
-      w = SiteProp.first.page_content_width
-      if img["width"] < w
+    def content_image_size(type='normal')
+
+      if type == 'normal'
+        img = MiniMagick::Image.open(image.thumb_big.path)
+        w = SiteProp.first.page_content_width
+        if img["width"] < w
+          img = MiniMagick::Image.open(image.thumb_small.path)
+          [img["width"],img["height"]]
+        else
+          [img["width"],img["height"]]
+        end
+
+      elsif type == 'small'
         img = MiniMagick::Image.open(image.thumb_small.path)
         [img["width"],img["height"]]
-      else
+
+      elsif type == 'big'
+        img = MiniMagick::Image.open(image.thumb_big.path)
         [img["width"],img["height"]]
       end
     end
