9161351f35cdb6c83df3a223b9a79b802f82eefd
C80609A <c080609a@gmail.com>
2017-06-26 09:15:00 +0300

    master: v. 0.1.0.14.

--- c80_news.gemspec
+++ c80_news.gemspec
@@ -19,10 +19,10 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
-  spec.add_development_dependency "bundler", "~> 1.9"
-  spec.add_development_dependency "rake", "~> 10.0"
-  spec.add_dependency 'activesupport', ['>= 3.0.0']
-  spec.add_dependency "rails", "~> 4.2.0"
+  spec.add_development_dependency "bundler"
+  spec.add_development_dependency "rake"
+  spec.add_dependency 'activesupport'
+  spec.add_dependency "rails"
   spec.add_dependency "local_time"
   spec.add_dependency "c80_lazy_images"
 end
--- db/seeds/c80_news_01_fill_props.rb
+++ db/seeds/c80_news_01_fill_props.rb
@@ -2,18 +2,18 @@
 
 C80News::Prop.delete_all
 C80News::Prop.create!({
-                                  thumb_lg_width: 640,
-                                  thumb_lg_height: 400,
+                          thumb_lg_width: 640,
+                          thumb_lg_height: 400,
 
-                                  thumb_md_width: 320,
-                                  thumb_md_height: 200,
+                          thumb_md_width: 320,
+                          thumb_md_height: 200,
 
-                                  thumb_sm_width: 80,
-                                  thumb_sm_height: 50,
-                                  
-                                  per_widget: 3,
-                                  per_page: 6,
-                                  preview_width: 250,
-                                  preview_height: 164
+                          thumb_sm_width: 80,
+                          thumb_sm_height: 50,
 
-                              })
+                          per_widget: 3,
+                          per_page: 6,
+                          thumb_preview_width: 250,
+                          thumb_preview_height: 164
+
+                      })
